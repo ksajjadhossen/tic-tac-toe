c8244b1bc7a08f488a85055b3392789e82e6f352
ksajjadhossen <ksajjadhossen07@gmail.com>
2025-08-21 19:58:10 +0600

    Winner calculating done

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -12,8 +12,18 @@ function Square({ value, onSquareClick }) {
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+    status = `Winner:${winner}`;
+  } else {
+    status = "Next Player" + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -25,6 +35,7 @@ export default function Board() {
   }
   return (
     <>
+      <div className="">{status}</div>
       <div className="flex">
         <Square
           value={squares[0]}
@@ -69,4 +80,30 @@ export default function Board() {
       </div>
     </>
   );
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 }
